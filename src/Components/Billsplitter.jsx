f9373c90583e7afc957e406145461de78bfea9f0
Irtaza Ahmed <irtazaahmedk@gmail.com>
2025-01-04 22:07:48 +0500

    logic done

--- src/Components/Billsplitter.jsx
+++ src/Components/Billsplitter.jsx
@@ -7,9 +7,8 @@ const Billsplitter = () => {
     const [friends, setFriends] = useState([])
     const [isModal, setIsModal] = useState(false)
     const [name, setName] = useState('')
+    const [id, setId] = useState('')
     const [box, setBox] = useState(false)
-    const [friendPay, setFriendPay] = useState('')
-    const [YouPay, setYouPay] = useState('')
 
     useEffect(() => {
         let friendList = localStorage.getItem('EatnSplitFriends')
@@ -37,10 +36,13 @@ const Billsplitter = () => {
     const handleChange = (e) => {
         setForm({ ...form, [e.target.name]: e.target.value })
     }
-    const handleBillForm = (e) => {
 
+
+    const handleBillForm = (e) => {
         setBillForm({ ...billForm, [e.target.name]: e.target.value })
     }
+
+
     const splitBill = () => {
         if (!billForm.Bill.trim() || !billForm.YourExpense.trim()) {
             return
@@ -50,6 +52,7 @@ const Billsplitter = () => {
         let YourExpense = parseInt(billForm.YourExpense)
         let friendExpense = parseInt(billForm[`${name}Expense`])
         let payer = billForm.payer ? billForm.payer : 'You'
+
         if (bill < YourExpense || bill < friendExpense) {
             return
         }
@@ -57,53 +60,39 @@ const Billsplitter = () => {
             return
         }
 
-        let user = friends.find((user) => user.name === name)
+        let user = friends.find((user) => user.id === id)
         if (payer === 'You') {
 
-            if (user.YouOwe) {
-                // ahzem owes me
-                let owes = user.YouOwe - friendExpense;
-                // 50 - 60
-                // -10
-
-
-                if (owes < 0) {
-                    // Reverse relationship: i owes ahzem
-                    user = { ...user, YouOwe: 0, owe: Math.abs(owes) };
-                } else {
-                    user = { ...user, YouOwe: owes };
-                }
-
-            } else {
-                // me owes ahzem
-                let owes = user.owe ? user.owe + friendExpense : friendExpense
-                user = { ...user, owe: owes }
-
-            }
-
-
-            setYouPay(true)
-
-        } else {
-
             if (user.owe) {
+                let owes = user.owe - friendExpense;
 
-                // payer ahzem
-                // me owes ahzem
-                let owes = user.owe - YourExpense;
-                // 60 - 80
-                //-20
+
                 if (owes < 0) {
-                    // Reverse relationship: ahzem owe me
-                    user = { ...user, owe: 0, YouOwe: Math.abs(owes) };
+                    user = { ...user, YouOwe: Math.abs(owes), owe: 0 };
                 } else {
                     user = { ...user, owe: owes };
                 }
 
             } else {
-                // ahzem owe me
-                let owes = user.YouOwe ? user.YouOwe + YourExpense : YourExpense
+                let owes = user.YouOwe ? user.YouOwe + friendExpense : friendExpense
                 user = { ...user, YouOwe: owes }
+
+            }
+
+
+        } else {
+
+            if (user.YouOwe) {
+                let owes = user.YouOwe - YourExpense;
+                if (owes < 0) {
+                    user = { ...user, owe: Math.abs(owes), YouOwe: 0 };
+                } else {
+                    user = { ...user, YouOwe: owes };
+                }
+
+            } else {
+                let owes = user.owe ? user.owe + YourExpense : YourExpense
+                user = { ...user, owe: owes }
 
             }
 
@@ -112,9 +101,15 @@ const Billsplitter = () => {
         }
 
         const updatedFriends = friends.map((friend) =>
-            friend.name === name ? user : friend
+            friend.id === id ? user : friend
         );
+
         localStorage.setItem('EatnSplitFriends', JSON.stringify(updatedFriends));
+
+        let friendList = localStorage.getItem('EatnSplitFriends')
+        if (friendList) {
+            setFriends(JSON.parse(friendList))
+        }
 
         setBox(false)
         setBillForm({ Bill: '', YourExpense: '', payer: '' })
@@ -122,57 +117,99 @@ const Billsplitter = () => {
     }
 
 
-    const handleSelectedFriend = (name) => {
+    const handleSelectedFriend = (name , id) => {
+        setId(id)
         setName(name)
+        setIsModal(false)
         setBox(true)
     }
 
+    const handleRemoveUser = (id)=> {
+        setBox(false)
+        const permission = window.confirm('do you want to remove this friend?')
+        if(!permission) {
+            return
+        }
+        const updatedList = friends.filter((friend)=> {
+            return friend.id !== id
+        })
+        localStorage.setItem('EatnSplitFriends', JSON.stringify(updatedList));
+        let friendList = localStorage.getItem('EatnSplitFriends')
+        if (friendList) {
+            setFriends(JSON.parse(friendList))
+        }
+        
+    }
+
     return (
-        <div className='flex gap-3'>
-            <div style={{ marginLeft: '20%', width: '310px' }} className='flex flex-col' >
+        <div className='flex gap-3 flex-wrap container-Box'>
+            <div style={{ minWidth: '310px' }} className='flex flex-col friendBox' >
                 <ul className='flex flex-col gap-3 '>
+                    {friends.length === 0 && <p className='text-sm'>No Friends to Show</p>}
                     {friends?.map((friend) => {
                         return <li key={friend.id} className='flex gap-2 items-center justify-between w-full'>
                             <div className='flex gap-2 items-center'>
 
-                                <img style={{ borderRadius: '100%', height: '30px' }} width='30px' src={friend.image} alt="friend" />
+                                <img style={{ borderRadius: '100%', height: '35px', width: '35px' }} src={friend.image} alt="friend" />
                                 <div className='text-sm '>
-                                    <p>{friend.name}</p>
+                                    <p className='font-medium'>{friend.name}</p>
                                     {!friend.owe && !friend.YouOwe ? <p>You and {friend.name} are evenly</p> : ''}
-                                    {friend.owe ? <p className='text-green-700 '>You owe {friend.name} {friend.owe}$</p> : ''}
-                                    {friend.YouOwe ? <p className='text-red-700'>{friend.name} owe You  {friend.YouOwe}$</p> : ''}
+                                    {friend.owe ? <p className='text-red-700 '>You owe {friend.name} {friend.owe}$</p> : ''}
+                                    {friend.YouOwe ? <p className='text-green-700'>{friend.name} owe You  {friend.YouOwe}$</p> : ''}
 
                                 </div>
                             </div>
-                            <button className='px-3 py-1 bg-orange-300 rounded-xl ms-3' onClick={() => handleSelectedFriend(friend.name)}>Select</button>
+                            <div className='flex gap-1'>
+
+                                <button className='px-3 py-1 bg-orange-300 rounded-xl ms-3 hover:bg-orange-400' onClick={() => handleSelectedFriend(friend.name, friend.id)}>Select</button>
+                                <button className='p-1 flex items-center' onClick={()=> handleRemoveUser(friend.id)}>
+                                    <lord-icon
+                                        src="https://cdn.lordicon.com/skkahier.json"
+                                        trigger="hover"
+                                        colors="primary:#c74b16"
+                                        style={{ width: '20px', height: '20px' }}>
+                                    </lord-icon>
+                                </button>
+                            </div>
                         </li>
                     })}
 
                 </ul>
-                <button className='px-3 py-1 bg-orange-400 rounded-xl self-end mt-3' onClick={() => setIsModal(prev => !prev)}>{isModal ? 'Close' : 'Add Friend'}</button>
+                <button className='px-3 py-1 bg-orange-400 rounded-xl self-end mt-3 hover:bg-orange-500' onClick={() => {
+                    setBox(false)
+                    setIsModal(prev => !prev)
+                }}>{isModal ? 'Close' : 'Add Friend'}</button>
 
                 {isModal && <div style={{ backgroundColor: '#ffd78d' }} className='p-2 flex flex-col mt-3 rounded-md'>
-                    <div className='flex gap-1 items-center mb-2'>
+                    <div className=' mb-2'>
 
-                        <label htmlFor="name" className='flex items-center text-sm'>
-                            <lord-icon
-                                src="https://cdn.lordicon.com/hroklero.json"
-                                trigger="hover"
-                                style={{ width: '17px', height: '17px' }}>
-                            </lord-icon>
-                            Friend name</label>
-                        <input value={form.name} name='name' onChange={handleChange} type="text" className='rounded-sm outline-none p-1 text-sm' />
+                        <label htmlFor="name" className='flex items-center text-sm justify-between'>
+                            <p className='flex items-center gap-0.5 text-sm'>
+
+                                <lord-icon
+                                    src="https://cdn.lordicon.com/hroklero.json"
+                                    trigger="hover"
+                                    style={{ width: '19px', height: '19px' }}>
+                                </lord-icon>
+                                Friend name
+                            </p>
+                            <input value={form.name} name='name' onChange={handleChange} type="text" className='rounded-sm outline-none p-1 text-sm' />
+                        </label>
                     </div>
-                    <div className='flex gap-1 items-center'>
-                        <label htmlFor="imageUrl" className='flex items-center text-sm'>
-                            <lord-icon
-                                src="https://cdn.lordicon.com/xtzwzauj.json"
-                                trigger="hover"
-                                colors="primary:#2ca58d,secondary:#4bb3fd,tertiary:#eee966,quaternary:#9ce5f4,quinary:#f9c9c0,senary:#ffc738"
-                                style={{ width: '17px', height: '17px' }}>
-                            </lord-icon>
-                            Image URL</label>
-                        <input value={form.imageUrl} name='imageUrl' onChange={handleChange} type="url" className='rounded-sm outline-none p-1 text-sm' />
+                    <div className=''>
+                        <label htmlFor="imageUrl" className='flex items-center text-sm justify-between'>
+                            <p className='flex items-center gap-0.5 text-sm'>
+
+                                <lord-icon
+                                    src="https://cdn.lordicon.com/xtzwzauj.json"
+                                    trigger="hover"
+                                    colors="primary:#2ca58d,secondary:#4bb3fd,tertiary:#eee966,quaternary:#9ce5f4,quinary:#f9c9c0,senary:#ffc738"
+                                    style={{ width: '19px', height: '19px' }}>
+                                </lord-icon>
+                                Image URL
+                            </p>
+                            <input value={form.imageUrl} name='imageUrl' onChange={handleChange} type="url" className='rounded-sm outline-none p-1 text-sm' />
+                        </label>
                     </div>
                     <button className='px-20 py-1 bg-orange-400 rounded-xl self-end mt-3' onClick={handleAddFriend}>Add</button>
                 </div>}
@@ -181,23 +218,66 @@ const Billsplitter = () => {
 
             </div>
 
-            {box && <div className='bg-orange-100 flex flex-col p-3' style={{ width: '370px' }}>
-                <h1>SPLIT A BILL WITH {name.toLocaleUpperCase()}</h1>
-                <label htmlFor="Bill">Bill Value
+            {box && <div className='bg-orange-100 flex flex-col gap-2 p-3' style={{ minWidth: '370px' }}>
+                <div className='flex justify-between' onClick={() => setBox(false)}>
 
-                    <input value={billForm.Bill} name='Bill' onChange={handleBillForm} type="number" />
+                    <h1>SPLIT A BILL WITH {name.toLocaleUpperCase()}</h1>
+                    <button>
+                        <lord-icon
+                            src="https://cdn.lordicon.com/zxvuvcnc.json"
+                            trigger="hover"
+                            colors="primary:#e86830"
+                            style={{ width: '25px', height: '25px' }}>
+                        </lord-icon>
+                    </button>
+                </div>
+                <label className='flex justify-between' htmlFor="Bill">
+                    <p className='flex items-center gap-0.5 text-sm'>
+                        <lord-icon
+                            src="https://cdn.lordicon.com/ymgusxed.json"
+                            trigger="hover"
+                            style={{ width: '19px', height: '19px' }}>
+                        </lord-icon>
+                        Bill Value
+                    </p>
+
+                    <input className='outline-none text-sm p-1 rounded-md' value={billForm.Bill} name='Bill' onChange={handleBillForm} type="number" />
                 </label>
-                <label htmlFor="YourExpense">Your expense
+                <label className='flex justify-between' htmlFor="YourExpense">
+                    <p className='flex items-center gap-0.5 text-sm'>
+                        <lord-icon
+                            src="https://cdn.lordicon.com/hroklero.json"
+                            trigger="hover"
+                            style={{ width: '19px', height: '19px' }}>
+                        </lord-icon>
+                        Your expense
+                    </p>
 
-                    <input value={billForm.YourExpense} name='YourExpense' onChange={handleBillForm} type="number" />
+                    <input className='outline-none text-sm p-1 rounded-md' value={billForm.YourExpense} name='YourExpense' onChange={handleBillForm} type="number" />
                 </label>
-                <label htmlFor={`${name}Expense`}>{name}'s expense
+                <label className='flex justify-between gap-2' htmlFor={`${name}Expense`}>
+                    <p className='flex items-center gap-0.5 text-sm'>
+                        <lord-icon
+                            src="https://cdn.lordicon.com/zdwrqfmb.json"
+                            trigger="hover"
+                            style={{ width: '19px', height: '19px' }}>
+                        </lord-icon>
+                        {name}'s expense
+                    </p>
 
-                    <input value={billForm[`${name}Expense`] || ''} name={`${name}Expense`} onChange={handleBillForm} type="number" />
+                    <input className='outline-none text-sm p-1 rounded-md' value={billForm[`${name}Expense`] || ''} name={`${name}Expense`} onChange={handleBillForm} type="number" />
                 </label>
-                <label htmlFor="payer">Who is paying the bill?
+                <label className='flex justify-between' htmlFor="payer">
+                    <p className='flex items-center gap-0.5 text-sm'>
+                        <lord-icon
+                            src="https://cdn.lordicon.com/unsfxkxg.json"
+                            trigger="hover"
+                            style={{ width: '19px', height: '19px' }}>
+                        </lord-icon>
+                        Who is paying the bill?
+                    </p>
 
-                    <select value={billForm.payer} name='payer' onChange={handleBillForm}>
+                    <select className='p-1 px-4 rounded-md text-sm outline-none' value={billForm.payer} name='payer' onChange={handleBillForm}>
                         <option value="You">You</option>
                         <option value={`${name}`}>{name}</option>
                     </select>
